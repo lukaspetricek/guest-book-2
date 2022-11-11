57cf024f302542dde546c1c96439d4f8097945a9
lukaspetricek <lukas.petricek.cz@gmail.com>
2022-11-11 12:57:37 +0100

    some methods added

---
+++ src/main/java/com/brights/guestbook2/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.brights.guestbook2.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+
+}
--- src/main/java/com/brights/guestbook2/service/UserServiceImpl.java
+++ src/main/java/com/brights/guestbook2/service/UserServiceImpl.java
@@ -1,28 +1,43 @@
 package com.brights.guestbook2.service;
 
 import com.brights.guestbook2.model.User;
+import com.brights.guestbook2.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 @SuppressWarnings("unused")
 public class UserServiceImpl implements UserService{
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public List<User> getAllUsers() {
-        return null;
+        return userRepository.findAll();
     }
 
     @Override
     public void saveUser(User user) {
-
+        this.userRepository.save(user);
     }
 
     @Override
     public User getUserById(long id) {
-        return null;
+        Optional<User> optional = this.userRepository.findById(id);  //impl. could be null or empty
+        User user = null;
+
+        if (optional.isPresent()) {
+            user = optional.get();
+        } else {
+            throw new RuntimeException("User with id: " + id + " does not exist.");
+        }
+        return user;
     }
 
     @Override
     public void deleteUserById(long id) {
-
+        this.userRepository.deleteById(id);
     }
 }
