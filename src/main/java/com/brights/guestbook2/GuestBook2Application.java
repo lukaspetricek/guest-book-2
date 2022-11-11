4a4558bde8150e2d5ec32d635ccadb160df15115
lukaspetricek <lukas.petricek.cz@gmail.com>
2022-11-11 11:48:51 +0100

    Initial commit

---
+++ src/main/java/com/brights/guestbook2/GuestBook2Application.java
@@ -0,0 +1,13 @@
+package com.brights.guestbook2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestBook2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestBook2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/brights/guestbook2/GuestBook2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brights.guestbook2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestBook2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
